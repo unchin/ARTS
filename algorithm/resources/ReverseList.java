f5544201e06f7b98b40aacfbf5f4e27cc5d1b76a
guoyongqin <isguoyq@gmail.com>
2021-08-20 16:36:26 +0800

    [UPD] 新增反转链表算法代码2.0

--- algorithm/resources/ReverseList.java
+++ algorithm/resources/ReverseList.java
@@ -7,12 +7,13 @@ public class ReverseList {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
-        while (curr != null) {
+        while (curr != null){
             ListNode next = curr.next;
             curr.next = prev;
             prev = curr;
             curr = next;
         }
+
         return prev;
     }
 
